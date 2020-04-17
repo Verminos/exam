626c7ca5bebb7d0f345b9af1e177d23952c371b2
verminos <verminos13@gmail.com>
2020-04-17 22:47:22 +0300

    sliders

---
+++ js/index.js
@@ -0,0 +1,68 @@
+var rev = $('.rev_slider');
+rev.on('init', function(event, slick, currentSlide) {
+  var
+    cur = $(slick.$slides[slick.currentSlide]),
+    next = cur.next(),
+    prev = cur.prev();
+  prev.addClass('slick-sprev');
+  next.addClass('slick-snext');
+  cur.removeClass('slick-snext').removeClass('slick-sprev');
+  slick.$prev = prev;
+  slick.$next = next;
+}).on('beforeChange', function(event, slick, currentSlide, nextSlide) {
+  console.log('beforeChange');
+  var
+    cur = $(slick.$slides[nextSlide]);
+  console.log(slick.$prev, slick.$next);
+  slick.$prev.removeClass('slick-sprev');
+  slick.$next.removeClass('slick-snext');
+  next = cur.next(),
+    prev = cur.prev();
+  prev.prev();
+  prev.next();
+  prev.addClass('slick-sprev');
+  next.addClass('slick-snext');
+  slick.$prev = prev;
+  slick.$next = next;
+  cur.removeClass('slick-next').removeClass('slick-sprev');
+});
+
+rev.slick({
+  speed: 1000,
+  arrows: true,
+  dots: false,
+  focusOnSelect: true,
+  infinite: true,
+  centerMode: true,
+  slidesPerRow: 1,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  centerPadding: '0',
+  swipe: true,
+  customPaging: function(slider, i) {
+    return '';
+  },
+  /*infinite: false,*/
+});
+
+$(document).ready(function(){
+	$('.slickSlaider').slick({
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		arrows: true,
+		dots: true,
+		dotsClass: "all__dots",
+		prevArrow: '<i class="fas fa-chevron-left arrow__left"></i>',
+		nextArrow: '<i class="fas fa-chevron-right arrow__right"></i>',
+	});
+});
+
+$(document).ready(function(){
+	$('.slaiderSlick').slick({
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		arrows: true,
+		prevArrow: '<i class="fas fa-chevron-left recent__btn pos__right"></i>',
+		nextArrow: '<i class="fas fa-chevron-right recent__btn pos__left"></i>',
+	});
+});
